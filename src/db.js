3f24a36ce4c99a6b40c625621047620f081400be
Chiara Lötscher <181711611+chiara-loetscher@users.noreply.github.com>
2024-11-01 16:12:46 +0100

    Add toggle favorite location db functionality

--- src/db.js
+++ src/db.js
@@ -12,5 +12,25 @@ export function createDB() {
 
   return {
     auth: new bbz307.Login("users", ["name", "password"], db),
-  };
+    toggleFavoriteLocation: async (userId, locationId) => toggleFavoriteLocation(db, userId, locationId)
+  }
+}
+
+const toggleFavoriteLocation = async (db, userId, locationId) => {
+  const { rows } = await db.query(
+    `SELECT 1 FROM favorites WHERE user_id = $1 AND location_id = $2`,
+    [userId, locationId]
+  );
+
+  if (rows.length === 0) {
+    await db.query(
+      `INSERT INTO favorites (user_id, location_id) VALUES ($1, $2)`,
+      [userId, locationId]
+    );
+  } else {
+    await db.query(
+      `DELETE FROM favorites WHERE user_id = $1 AND location_id = $2`,
+      [userId, locationId]
+    );
+  }
 }
