06074e4803d909fd7731593aaa721a3c31c6a5cb
Chiara Lötscher <181711611+chiara-loetscher@users.noreply.github.com>
2024-11-05 13:54:39 +0100

    Refactor db & server to be classes

--- src/db.js
+++ src/db.js
@@ -1,129 +1,163 @@
 import pgPool from "pg";
 import bbz307 from "bbz307";
 
-export function createDB() {
-  const db = new pgPool.Pool({
-    user: "crimson_dawn_5481",
-    host: "bbz.cloud",
-    database: "crimson_dawn_5481",
-    password: "896e8f91cd48506b11bd3628acc9b102",
-    port: 30211,
-  });
-
-  return {
-    auth: new bbz307.Login("users", ["name", "password"], db),
-    locations: locations(db),
-    categories: categories(db),
-    favorites: favorites(db),
-    reviews: reviews(db),
+export default class Database {
+  constructor() {
+    this.db = new pgPool.Pool({
+      user: "crimson_dawn_5481",
+      host: "bbz.cloud",
+      database: "crimson_dawn_5481",
+      password: "896e8f91cd48506b11bd3628acc9b102",
+      port: 30211,
+    });
+    this.auth = new bbz307.Login("users", ["name", "password"], this.db);
   }
+
+  categories = {
+    getAll: async () => (await this.db.query(`SELECT * FROM categories`)).rows,
+  };
+
+  reviews = {
+    getByUser: async (userId) =>
+      (
+        await this.db.query(`SELECT * FROM reviews WHERE user_id = $1`, [
+          userId,
+        ])
+      ).rows,
+    getByLocation: async (locationId) =>
+      (
+        await this.db.query(
+          `
+      SELECT * FROM reviews r JOIN users u ON r.user_id = u.id 
+      WHERE location_id = $1`,
+          [locationId]
+        )
+      ).rows,
+    create: async (userId, locationId, rating, comment) => {
+      const { rowCount } = await this.db.query(
+        `SELECT 1 FROM reviews WHERE user_id = $1 AND location_id = $2`,
+        [userId, locationId]
+      );
+
+      if (rowCount === 0) {
+        await this.db.query(
+          `INSERT INTO reviews (user_id, location_id, rating, comment) VALUES ($1, $2, $3, $4)`,
+          [userId, locationId, rating, comment]
+        );
+      } else {
+        await this.db.query(
+          `UPDATE reviews SET rating = $3, comment = $4 WHERE user_id = $1 AND location_id = $2`,
+          [userId, locationId, rating, comment]
+        );
+      }
+    },
+  };
+
+  favorites = {
+    getByUser: async (userId) =>
+      (
+        await this.db.query(`SELECT * FROM favorites WHERE user_id = $1`, [
+          userId,
+        ])
+      ).rows,
+    toggle: async (userId, locationId) => {
+      const { rowCount } = await this.db.query(
+        `SELECT 1 FROM favorites WHERE user_id = $1 AND location_id = $2`,
+        [userId, locationId]
+      );
+
+      if (rowCount === 0) {
+        await this.db.query(
+          `INSERT INTO favorites (user_id, location_id) VALUES ($1, $2)`,
+          [userId, locationId]
+        );
+      } else {
+        await this.db.query(
+          `DELETE FROM favorites WHERE user_id = $1 AND location_id = $2`,
+          [userId, locationId]
+        );
+      }
+    },
+  };
+
+  locations = {
+    getAll: async (userId) => {
+      const { rows: locations } = await this.db.query(`
+        SELECT l.id AS id,
+          l.name AS name,
+          l.street AS street,
+          l.house_number AS "houseNumber",
+          l.zip_code AS "zipCode",
+          l.place AS place,
+          l.country AS country,
+          l.category_id AS category_id,
+          ROUND(COALESCE(AVG(r.rating), 0), 1) AS rating
+        FROM locations l LEFT JOIN reviews r ON l.id = r.location_id
+        GROUP BY l.id, l.name`);
+      const myReviews = await this.reviews.getByUser(userId);
+      const allCategories = await this.categories.getAll();
+      const myFavorites = await this.favorites.getByUser(userId);
+      for (const location of locations) {
+        location.category = allCategories.find(
+          (category) => category.id === location.category_id
+        ).name;
+        location.isFavorited = myFavorites.some(
+          (favorite) =>
+            favorite.location_id === location.id && favorite.user_id === userId
+        );
+        location.myReview = myReviews.find(
+          (review) =>
+            review.location_id === location.id && review.user_id === userId
+        );
+        location.reviews = await this.reviews.getByLocation(location.id);
+      }
+      return locations;
+    },
+    getFavorites: async (userId) => {
+      const { rows: locations } = await this.db.query(
+        `
+        SELECT l.id AS id,
+          l.name AS name,
+          l.street AS street,
+          l.house_number AS "houseNumber",
+          l.zip_code AS "zipCode",
+          l.place AS place,
+          l.country AS country,
+          l.category_id AS category_id,
+          ROUND(COALESCE(AVG(r.rating), 0), 1) AS rating
+        FROM locations l JOIN favorites f ON l.id = f.location_id
+        LEFT JOIN reviews r ON f.location_id = r.location_id WHERE f.user_id = $1
+        GROUP BY l.id, l.name`,
+        [userId]
+      );
+      const myReviews = await this.reviews.getByUser(userId);
+      const allCategories = await this.categories.getAll();
+      for (const location of locations) {
+        location.category = allCategories.find(
+          (category) => category.id === location.category_id
+        ).name;
+        location.isFavorited = true;
+        location.myReview = myReviews.find(
+          (review) =>
+            review.location_id === location.id && review.user_id === userId
+        );
+        location.reviews = await this.reviews.getByLocation(location.id);
+      }
+      return locations;
+    },
+    create: async (
+      name,
+      street,
+      houseNumber,
+      zipCode,
+      place,
+      country,
+      categoryId
+    ) => {
+      await this.db.query(
+        `INSERT INTO locations (name, street, house_number, zip_code, place, country, category_id) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+        [name, street, houseNumber, zipCode, place, country, categoryId]
+      );
+    },
+  };
 }
-
-const categories = (db) => ({
-  getAll: async () => (await db.query(`SELECT * FROM categories`)).rows,
-})
-
-const reviews = (db) => ({
-  getByUser: async (userId) => (await db.query(`SELECT * FROM reviews WHERE user_id = $1`, [userId])).rows,
-  getByLocation: async (locationId) => (await db.query(`
-    SELECT * FROM reviews r JOIN users u ON r.user_id = u.id 
-    WHERE location_id = $1`, [locationId])).rows,
-  create: async (userId, locationId, rating, comment) => {
-    const { rowCount } = await db.query(
-      `SELECT 1 FROM reviews WHERE user_id = $1 AND location_id = $2`,
-      [userId, locationId]
-    );
-  
-    if (rowCount === 0) {
-      await db.query(
-        `INSERT INTO reviews (user_id, location_id, rating, comment) VALUES ($1, $2, $3, $4)`,
-        [userId, locationId, rating, comment]
-      );
-    }
-    else {
-      await db.query(
-        `UPDATE reviews SET rating = $3, comment = $4 WHERE user_id = $1 AND location_id = $2`,
-        [userId, locationId, rating, comment]
-      );
-    }
-  },
-})
-
-const favorites = (db) => ({
-  getByUser: async (userId) => (await db.query(`SELECT * FROM favorites WHERE user_id = $1`, [userId])).rows,
-  toggle: async (userId, locationId) => {
-    const { rowCount } = await db.query(
-      `SELECT 1 FROM favorites WHERE user_id = $1 AND location_id = $2`,
-      [userId, locationId]
-    );
-  
-    if (rowCount === 0) {
-      await db.query(
-        `INSERT INTO favorites (user_id, location_id) VALUES ($1, $2)`,
-        [userId, locationId]
-      );
-    } else {
-      await db.query(
-        `DELETE FROM favorites WHERE user_id = $1 AND location_id = $2`,
-        [userId, locationId]
-      );
-    }
-  }
-})
-
-const locations = (db) => ({
-  getAll: async (userId) =>  {
-    const { rows: locations } = await db.query(`
-      SELECT l.id AS id,
-        l.name AS name,
-        l.street AS street,
-        l.house_number AS "houseNumber",
-        l.zip_code AS "zipCode",
-        l.place AS place,
-        l.country AS country,
-        l.category_id AS category_id,
-        ROUND(COALESCE(AVG(r.rating), 0), 1) AS rating
-      FROM locations l LEFT JOIN reviews r ON l.id = r.location_id
-      GROUP BY l.id, l.name`);
-    const myReviews = await reviews(db).getByUser(userId);
-    const allCategories = await categories(db).getAll();
-    const myFavorites = await favorites(db).getByUser(userId);
-    for (const location of locations) {
-      location.category = allCategories.find(category => category.id === location.category_id).name;
-      location.isFavorited = myFavorites.some(favorite => favorite.location_id === location.id && favorite.user_id === userId);
-      location.myReview = myReviews.find(review => review.location_id === location.id && review.user_id === userId);
-      location.reviews = await reviews(db).getByLocation(location.id);
-    }
-    return locations;
-  },
-  getFavorites: async (userId) => {
-    const { rows: locations } = await db.query(`
-      SELECT l.id AS id,
-        l.name AS name,
-        l.street AS street,
-        l.house_number AS "houseNumber",
-        l.zip_code AS "zipCode",
-        l.place AS place,
-        l.country AS country,
-        l.category_id AS category_id,
-        ROUND(COALESCE(AVG(r.rating), 0), 1) AS rating
-      FROM locations l JOIN favorites f ON l.id = f.location_id
-      LEFT JOIN reviews r ON f.location_id = r.location_id WHERE f.user_id = $1
-      GROUP BY l.id, l.name`, [userId]);
-    const myReviews = await reviews(db).getByUser(userId);
-    const allCategories = await categories(db).getAll();
-    for (const location of locations) {
-      location.category = allCategories.find(category => category.id === location.category_id).name;
-      location.isFavorited = true;
-      location.myReview = myReviews.find(review => review.location_id === location.id && review.user_id === userId);
-      location.reviews = await reviews(db).getByLocation(location.id);
-    }
-    return locations;
-  },
-  create: async (db, name, street, houseNumber, zipCode, place, country, categoryId) => {
-    await db.query(
-      `INSERT INTO locations (name, street, house_number, zip_code, place, country, category_id) VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-      [name, street, houseNumber, zipCode, place, country, categoryId]
-    );
-  }
-})
--- src/server.js
+++ src/server.js
@@ -1,69 +1,71 @@
 import express from "express";
-import { engine } from "express-handlebars";
+import expressHandlebars from "express-handlebars";
 import sessions from "express-session";
 import path from "path";
 import fs from "fs";
 
-export function createServer() {
-  const server = express();
+export default class Server {
+  constructor() {
+    const server = express();
 
-  server.use(express.static("public"));
-  server.use(express.urlencoded({ extended: true }));
+    server.use(express.static("public"));
+    server.use(express.urlencoded({ extended: true }));
 
-  server.engine("handlebars", ENGINE);
-  server.set("view engine", "handlebars");
+    server.engine("handlebars", this.engine);
+    server.set("view engine", "handlebars");
 
-  server.use(
-    sessions({
-      secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-      saveUninitialized: true,
-      cookie: { maxAge: 86400000, secure: false },
-      resave: false,
-    })
-  );
+    server.use(
+      sessions({
+        secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+        saveUninitialized: true,
+        cookie: { maxAge: 86400000, secure: false },
+        resave: false,
+      })
+    );
 
-  return server;
+    return server;
+  }
+
+  engine = expressHandlebars.engine({
+    helpers: {
+      icon: function (name) {
+        try {
+          // Read the SVG file from the public/icons directory
+          const iconPath = path.resolve("public/icons", name + ".svg");
+          // Return the content of the SVG file
+          return fs.readFileSync(iconPath, "utf8");
+        } catch (err) {
+          console.error("Error reading SVG file:", err);
+          return "";
+        }
+      },
+      stars: function (rating) {
+        let fullStars = Math.floor(rating);
+        let halfStar = rating % 1 >= 0.5 ? 1 : 0;
+        let emptyStars = 5 - fullStars - halfStar;
+
+        let stars = [];
+
+        // full stars
+        for (let i = 0; i < fullStars; i++) {
+          stars.push("star-full");
+        }
+
+        // half star if applicable
+        if (halfStar) {
+          stars.push("star-half-full");
+        }
+
+        // empty stars
+        for (let i = 0; i < emptyStars; i++) {
+          stars.push("star");
+        }
+
+        return stars;
+      },
+      eq: function (a, b) {
+        return a === b;
+      },
+    },
+  });
 }
-
-const ENGINE = engine({
-  helpers: {
-    icon: function (name) {
-      try {
-        // Read the SVG file from the public/icons directory
-        const iconPath = path.resolve("public/icons", name + ".svg");
-        // Return the content of the SVG file
-        return fs.readFileSync(iconPath, "utf8");
-      } catch (err) {
-        console.error("Error reading SVG file:", err);
-        return "";
-      }
-    },
-    stars: function (rating) {
-      let fullStars = Math.floor(rating);
-      let halfStar = rating % 1 >= 0.5 ? 1 : 0;
-      let emptyStars = 5 - fullStars - halfStar;
-
-      let stars = [];
-
-      // full stars
-      for (let i = 0; i < fullStars; i++) {
-        stars.push("star-full");
-      }
-
-      // half star if applicable
-      if (halfStar) {
-        stars.push("star-half-full");
-      }
-
-      // empty stars
-      for (let i = 0; i < emptyStars; i++) {
-        stars.push("star");
-      }
-
-      return stars;
-    },
-    eq: function (a, b) {
-      return a === b;
-    },
-  },
-});
